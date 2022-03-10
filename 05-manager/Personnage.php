856b7d1df87610295ab59f3e734dfd988ede2eac
Mikhawa <michaeljpitz@gmail.com>
2022-03-10 12:05:03 +0100

    PHP 8 - class

--- 05-manager/Personnage.php
+++ 05-manager/Personnage.php
@@ -14,6 +14,147 @@ class Personnage
     // constantes Modificateur de visibilité des constantes de classe, à partir de PHP 7.1 : https://www.php.net/manual/fr/language.oop5.constants.php
     public const VIE_DE_BASE = 1000;
 
+
     // méthodes
+
+
+    // constructeur
+    public function __construct()
+    {
+
+    }
+
+
+    // getters
+
+    /**
+     * @return int
+     */
+    public function getIdPersonnage(): int
+    {
+        return $this->idPersonnage;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @return int
+     */
+    public function getForcePerso(): int
+    {
+        return $this->forcePerso;
+    }
+
+    /**
+     * @return float
+     */
+    public function getDegats(): float
+    {
+        return $this->degats;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNiveau(): int
+    {
+        return $this->niveau;
+    }
+
+    /**
+     * @return int
+     */
+    public function getExperience(): int
+    {
+        return $this->experience;
+    }
+
+    /**
+     * @return int
+     */
+    public function getVie(): int
+    {
+        return $this->vie;
+    }
+
+    /**
+     * @param int $idPersonnage
+     * @return Personnage
+     */
+    public function setIdPersonnage(int $idPersonnage): Personnage
+    {
+        $this->idPersonnage = $idPersonnage;
+        return $this;
+    }
+
+
+    // setters
+
+    /**
+     * @param string $nom
+     * @return Personnage
+     */
+    public function setNom(string $nom): Personnage
+    {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    /**
+     * @param int $forcePerso
+     * @return Personnage
+     */
+    public function setForcePerso(int $forcePerso): Personnage
+    {
+        $this->forcePerso = $forcePerso;
+        return $this;
+    }
+
+    /**
+     * @param float $degats
+     * @return Personnage
+     */
+    public function setDegats(float $degats): Personnage
+    {
+        $this->degats = $degats;
+        return $this;
+    }
+
+    /**
+     * @param int $niveau
+     * @return Personnage
+     */
+    public function setNiveau(int $niveau): Personnage
+    {
+        $this->niveau = $niveau;
+        return $this;
+    }
+
+    /**
+     * @param int $experience
+     * @return Personnage
+     */
+    public function setExperience(int $experience): Personnage
+    {
+        $this->experience = $experience;
+        return $this;
+    }
+
+    /**
+     * @param int $vie
+     * @return Personnage
+     */
+    public function setVie(int $vie): Personnage
+    {
+        $this->vie = $vie;
+        return $this;
+    }
+
 
 }

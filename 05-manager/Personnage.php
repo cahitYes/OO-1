8a36c2694721746e131c2254d07e60f58d0e72ab
mikhawa <michaeljpitz@gmail.com>
2022-03-14 11:36:20 +0100

    orthographe

--- 05-manager/Personnage.php
+++ 05-manager/Personnage.php
@@ -2,7 +2,7 @@
 
 class Personnage
 {
-    // Attributs ou propriétés, en général toujours private (ou protected qu'on verra lors des extends) - A partir de PHP 7.4, on type nos attributs : https://www.php.net/manual/fr/migration74.new-features.php
+    // Attributs ou propriétés, en général toujours private (ou protected qu'on verra lors des extends) - À partir de PHP 7.4, on type nos attributs : https://www.php.net/manual/fr/migration74.new-features.php
     private int $idPersonnage;
     private string $nom;
     private int $forcePerso;
@@ -11,7 +11,7 @@ class Personnage
     private int $experience;
     private int $vie;
 
-    // constantes Modificateur de visibilité des constantes de classe, à partir de PHP 7.1 : https://www.php.net/manual/fr/language.oop5.constants.php
+    // constantes Modificatrices de visibilité des constantes de classe, à partir de PHP 7.1 : https://www.php.net/manual/fr/language.oop5.constants.php
     public const VIE_DE_BASE = 1000;
 
 

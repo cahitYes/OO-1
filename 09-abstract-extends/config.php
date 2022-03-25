7d1b486b7492127efe91d11aa1144c4b140d3db5
cahitYes <efeyesiloglu@hotmail.com>
2022-03-25 09:37:41 +0100

    myPDO

---
+++ 09-abstract-extends/config.php
@@ -0,0 +1,13 @@
+<?php
+// DB
+define("DB_TYPE","mysql");
+define("DB_HOST","localhost");
+define("DB_PORT",3307);
+define("DB_NAME","jeuxoo");
+define("DB_CHARSET","utf8");
+define("DB_LOGIN","root");
+define("DB_PWD","");
+
+// production-> $prod=true
+
+$prod = false;
---
+++ 09-abstract-extends/index.php
@@ -0,0 +1,24 @@
+<?php
+require_once "modele/MyPDO.php";
+require_once "modele/PersonnageManager.php";
+require_once "modele/Personnage.php";
+require_once "config.php";
+
+try {
+    $connectPDO = new PDO(DB_TYPE . ':dbname=' . DB_NAME . ';host=' . DB_HOST . ';charset=' . DB_CHARSET . ';port=' . DB_PORT, DB_LOGIN, DB_PWD);
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+var_dump($connectPDO);
+
+try {
+    $connectMyPDO = new MyPDO(DB_TYPE . ':dbname=' . DB_NAME . ';host=' . DB_HOST . ';charset=' . DB_CHARSET . ';port=' . DB_PORT, DB_LOGIN, DB_PWD, null);
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+var_dump($connectMyPDO);
+
+
+
+
+?>
